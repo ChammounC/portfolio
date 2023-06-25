accec087c80c5c76f53560481842febe9dab6fd6
ChammounC <chingchakhap11@gmail.com>
2023-06-25 15:32:53 +0530

    remove 'hydrateRoot' from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,7 @@
-import { hydrateRoot } from 'react-dom'
 import  ReactDOM  from 'react-dom/client'
 import App from './app'
 
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement);
 
-if (rootElement.hasChildNodes()) {
-    hydrateRoot(<App />, rootElement)
-} else {
-    root.render(<App />)
-}
+root.render(<App />)
